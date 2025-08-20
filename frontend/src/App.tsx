ee4762252876f17e3fa567c5e520e7a2e664e40b
Abin368 <abinmathew3720@gmail.com>
2025-08-20 19:33:48 +0530

    chords: folder structure after shadcn setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,8 @@ export default function App() {
 
 
   return (
-   <>
-   
-   
-   </>
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white text-3xl font-bold">
+      🚀 Tailwind v4 is workihhhdfweng!
+    </div>
   )
 }
