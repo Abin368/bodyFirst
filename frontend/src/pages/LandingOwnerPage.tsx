f4a63d9073767de8c8c4f4d06293223d8eda98c3
Abin368 <abinmathew3720@gmail.com>
2025-08-21 17:10:40 +0530

    feat: setup frontend routes (AppRoutes, AuthRoutes, PrivateRoute)

---
+++ frontend/src/pages/LandingOwnerPage.tsx
@@ -0,0 +1,16 @@
+
+import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer'
+const LandingOwnerPage = () => {
+  return (
+    <>
+    <Header/>
+
+
+
+    <Footer/>
+    </>
+  )
+}
+
+export default LandingOwnerPage
