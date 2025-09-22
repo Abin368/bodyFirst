0545128d688dfb933f9372ff2c7574bc2fc16a17
Abin Mathew <abinmathew3720@gmail.com>
2025-09-22 21:11:05 +0530

    feat:after setting docker for the review

--- frontend/src/schemas/gym.ts
+++ frontend/src/schemas/gym.ts
@@ -1,18 +1,17 @@
-
 import z from 'zod'
 
 export const GymSchema = z.object({
-    name:z.string().min(2,'Gym name required'),
-    contactNo:z.string().min(10,'Invalid Cuntact number'),
-    website:z.string().url().optional(),
-    address:z.object({
-        city:z.string(),
-        state:z.string(),
-        street:z.string(),
-        pincode:z.string()
-    }),
-    images:z.array(z.string()).optional(),
-    services:z.array(z.string()).optional()
+  name: z.string().min(2, 'Gym name required'),
+  contactNo: z.string().min(10, 'Invalid Cuntact number'),
+  website: z.string().url().optional(),
+  address: z.object({
+    city: z.string(),
+    state: z.string(),
+    street: z.string(),
+    pincode: z.string(),
+  }),
+  images: z.array(z.string()).optional(),
+  services: z.array(z.string()).optional(),
 })
 
 export type GymFormValues = z.infer<typeof GymSchema>
